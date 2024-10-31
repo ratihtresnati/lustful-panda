9ae0927e0e0b6bb7fe0b95869d5a6c67e930f699
Myrth <74478820+ratihtresnati@users.noreply.github.com>
2024-11-01 03:38:30 +0700

    add setting menu

--- Assets/Scripts/Input System/InputManager.cs
+++ Assets/Scripts/Input System/InputManager.cs
@@ -10,8 +10,10 @@ public class InputManager : MonoBehaviour
     public static PlayerInputManager PlayerInputManager;
     public bool MenuOpenInput { get; private set; }
     public bool MenuCloseInput { get; private set; }
+    public bool ButtonClickInput { get; private set; }
     private InputAction _menuOpenAction;
     private InputAction _menuCloseAction;
+    private InputAction _selectAction;
     private void Awake()
     {
         if(instance == null)
@@ -23,11 +25,13 @@ public class InputManager : MonoBehaviour
         PlayerInput = gameObject.GetComponent<PlayerInput>();
         _menuOpenAction = PlayerInput.actions["MenuOpen"];
         _menuCloseAction = PlayerInput.actions["MenuClose"];
+        _selectAction = PlayerInput.actions["Click"];
     }
 
     private void Update()
     {
         MenuOpenInput = _menuOpenAction.WasPressedThisFrame();
         MenuCloseInput = _menuCloseAction.WasPressedThisFrame();
+        ButtonClickInput = _selectAction.WasPressedThisFrame();
     }
 }
