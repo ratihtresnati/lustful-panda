d6820704c8c74392ee4e0aca605d5a96523dfebf
Aezakmi <andrearamadhan123@gmail.com>
2024-11-05 23:19:44 +0700

    complete ai zoo keeper for now

--- Assets/Scripts/AI Script/AIPatrolling.cs
+++ Assets/Scripts/AI Script/AIPatrolling.cs
@@ -8,7 +8,10 @@ public class AIPatrolling : MonoBehaviour
 {
     public AISensor Sensor;
 
+    public AIAnimatorController AIAnimatorController;
+
     [SerializeField] private Transform player;
+    [SerializeField] private float walkSpeed = 3f;
     [SerializeField] private float runSpeed = 4f;
 
     NavMeshAgent agent;
@@ -18,12 +21,13 @@ public class AIPatrolling : MonoBehaviour
     ZooKeeperState currentState;
 
     [SerializeField] private float idleTime = 2f;
-    [SerializeField] private float idleTimeAfterLosePlayer = 5f;
+    [SerializeField] private float idleTimeAfterLosePlayer = 3f;
     float idleTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        AIAnimatorController = GetComponent<AIAnimatorController>();
         agent = GetComponent<NavMeshAgent>();
         Sensor = GetComponent<AISensor>();
 
@@ -34,20 +38,47 @@ public class AIPatrolling : MonoBehaviour
     
     void Update()
     {
+       
 
         switch (currentState)
         {
             case ZooKeeperState.Idle:
                 Idle();
+                AIAnimatorController.Idle();
+                break;
+            case ZooKeeperState.Search:
+                Search();
+                AIAnimatorController.Idle();
                 break;
             case ZooKeeperState.Patrol:
                 Patrol();
+                AIAnimatorController.Walk();
+                break;
+            case ZooKeeperState.AfterChase:
+                AfterChase();
                 break;
             case ZooKeeperState.Chase:
                 Chase();
+                AIAnimatorController.Run();
                 break;
         }
 
+    }
+
+    private void AfterChase()
+    {
+        idleTimer -= Time.deltaTime;
+
+        if (idleTimer <= 0f)
+        {
+            idleTimer = idleTimeAfterLosePlayer;
+            currentState = ZooKeeperState.Idle;
+        }
+        if (Sensor.canSeePlayer)
+        {
+            currentState = ZooKeeperState.Chase;
+
+        }
     }
 
     void Chase()
@@ -56,14 +87,38 @@ public class AIPatrolling : MonoBehaviour
         agent.SetDestination(player.position);
         if (!Sensor.canSeePlayer)
         {
-            idleTimer = idleTimeAfterLosePlayer;
-            currentState = ZooKeeperState.Idle;
+            idleTimer = 2f;
+
+            currentState = ZooKeeperState.AfterChase;
 
         }
     }
 
     private void Idle()
     {
+
+        GetComponent<NavMeshAgent>().speed = walkSpeed;
+        idleTimer -= Time.deltaTime;
+
+        if (idleTimer <= 0f)
+        {
+            target = patrolPoint[patrolPointIndex].position;
+            agent.SetDestination(target);
+
+            currentState = ZooKeeperState.Patrol;
+        }
+
+        if (Sensor.canSeePlayer)
+        {
+            currentState = ZooKeeperState.Chase;
+
+        }
+    }
+
+    private void Search()
+    {
+
+        GetComponent<NavMeshAgent>().speed = walkSpeed;
         idleTimer -= Time.deltaTime;
 
         if (idleTimer <= 0f)
@@ -81,6 +136,7 @@ public class AIPatrolling : MonoBehaviour
     }
     private void Patrol()
     {
+        GetComponent<NavMeshAgent>().speed = walkSpeed;
         if (agent.remainingDistance <= 0.5f)
         {
             patrolPointIndex++;
@@ -93,19 +149,21 @@ public class AIPatrolling : MonoBehaviour
             }
                 currentState = ZooKeeperState.Idle;
 
+
+        }
             if (Sensor.canSeePlayer)
             {
                 currentState = ZooKeeperState.Chase;
             }
-
-        }
     }
 
     public enum ZooKeeperState
     {
         Idle,
         Patrol,
-        Chase
+        Chase,
+        Search,
+        AfterChase
     }
 
 }
