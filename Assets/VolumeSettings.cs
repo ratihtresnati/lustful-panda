2cf39eefa767ce17b81c2ae710de2196459e07e4
Thirmd <muhfathir27@gmail.com>
2024-11-18 15:41:08 +0700

    update audio control & roll input system

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -20,18 +20,47 @@ public class VolumeSettings : MonoBehaviour
             SetMusicVolume();
         }
     }
+
     public void SetMusicVolume(){
-        float volume = musicSlider.value;
-        audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        float volume;
+
+        if (musicSlider == null) {
+            if (PlayerPrefs.HasKey("musicVolume")) {
+                volume = PlayerPrefs.GetFloat("musicVolume");
+            } else {
+                volume = 0.5f;
+            }
+        } else {
+            volume = musicSlider.value;
+        }
+
+        audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("musicVolume", volume);
     }
     public void SetSFXVolume(){
-        float volume = sfxSlider.value;
-        audioMixer.SetFloat("sfx", Mathf.Log10(volume)*20);
+        float volume;
+
+        if (sfxSlider == null) {
+            if (PlayerPrefs.HasKey("sfxVolume")) {
+                volume = PlayerPrefs.GetFloat("sfxVolume");
+            } else {
+                volume = 0.5f;
+            }
+        } else {
+            volume = sfxSlider.value;
+        }
+
+        audioMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("sfxVolume", volume);
     }
 
     public void LoadVolume(){
+        if (musicSlider == null && sfxSlider == null) {
+            SetMusicVolume();
+            SetSFXVolume();
+            return;
+        }
+
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
 
